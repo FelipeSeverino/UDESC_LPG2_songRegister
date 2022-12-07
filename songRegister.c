712aebbadd434b67e95e28fdc8ec826156bfdb6f
FelipeSeverino <felipe.carlos.2027@gmail.com>
2022-12-07 17:01:35 -0300

    Criando mensagem de iniciando

--- songRegister.c
+++ songRegister.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  
+    printf("----------------------");
+    printf("Iniciando songRegister");
+    printf("----------------------");
   
     return 0;
 }
